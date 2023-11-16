6b7b63257707375fc2b55e9eb2f20839c977e02a
Kleppir <danielsnaerh@gmail.com>
2023-11-16 23:48:50 +0000

    Initial push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CardInfo from './CardInfo';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Card Information</h1>
+            <CardInfo />
+        </div>
+    );
 }
 
+ReactDOM.render(<App />, document.getElementById('root'));
 export default App;
